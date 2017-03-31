aa49a95a355c078bcb82be7e9867201318bf1fb5
Mike <mike.sears@marqueone.net>
2017-03-31 16:59:28 -0400

    starting to plan out frontend application

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from "tam/components/app";
+
+import "tam/resources/styles/tam.css";
 
 ReactDOM.render(
   <App />,
